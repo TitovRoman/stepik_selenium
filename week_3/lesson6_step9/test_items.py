dc2b3871596c5ec5b6c34d7b30a14c8dd4e15fdb
TitovRoman <60462771+TitovRoman@users.noreply.github.com>
2021-06-27 12:16:21 +0300

    Update test_items.py

--- week_3/lesson6_step9/test_items.py
+++ week_3/lesson6_step9/test_items.py
@@ -4,6 +4,6 @@ import time
 def test_add_to_basket_button(browser):
     url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(url)
-    time.sleep(4)
+#     time.sleep(4)
     button = browser.find_elements_by_class_name('btn-add-to-basket')
     assert len(button) == 1, 'The page must have one add to cart button'
