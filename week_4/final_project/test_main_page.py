e307b4870bdb6165dff1eaa8247770e4d2f10e16
Roman Titov <romiktitov@gmail.com>
2021-06-28 10:45:17 +0300

    added LoginPage

--- week_4/final_project/test_main_page.py
+++ week_4/final_project/test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -7,8 +8,16 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_login_page_validation(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
